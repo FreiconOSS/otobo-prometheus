3e2bbb26cd1591f187d99aa3159235ed23226927
André Randel <arandel@freicon.de>
2021-02-26 14:26:55 +0100

    added missing files

---
+++ Custom/Kernel/System/Prometheus.pm
@@ -0,0 +1,44 @@
+package Kernel::System::Prometheus;
+
+use strict;
+use warnings;
+
+use Kernel::System::DateTime;
+
+use Kernel::System::VariableCheck qw( IsArrayRefWithData IsHashRefWithData );
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Log',
+);
+
+
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    $Self->{Debug} = 0;
+    $Self->{client} = Net::Prometheus->new;
+    return $Self;
+}
+
+
+sub new_gauge {
+    my ( $Self, %Param ) = @_;
+    return $Self->{client}->new_gauge(%Param);
+}
+
+sub new_counter {
+    my ( $Self, %Param ) = @_;
+    return $Self->{client}->new_counter(%Param);
+}
+
+sub render {
+    my ( $Self, %Param ) = @_;
+    return $Self->{client}->render(%Param);
+}
+
+1;
